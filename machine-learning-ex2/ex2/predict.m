42d5319daf1c4a5652378392f09022d47b5a41e3
Ling <lingd1944@gmail.com>
2018-09-28 21:52:10 -0400

    Add files via upload

---
+++ machine-learning-ex2/ex2/predict.m
@@ -0,0 +1,11 @@
+function p = predict(theta, X)
+
+m = size(X, 1); % Number of training examples
+
+% You need to return the following variables correctly
+p = zeros(m, 1);
+
+% ====================== YOUR CODE HERE ======================
+p = sigmoid(X * theta) >= 0.5;
+% =========================================================================
+end
